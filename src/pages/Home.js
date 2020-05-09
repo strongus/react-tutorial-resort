8b16b59a2c258df5bf3b2eacad12f08121cf2ef3
Kent Strong <kstrong@k12connect.com>
2020-05-09 16:30:41 -0400

    Setup Hero component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,6 @@
-import React from 'react'
+import React from "react";
+import Hero from "../components/Hero";
 
 export default function Home() {
-    return (
-        <div>
-            hello from homepage
-        </div>
-    )
+  return <Hero />;
 }
--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,10 +1,6 @@
-import React from 'react'
-
+import React from "react";
+import Hero from "../components/Hero";
 export const Rooms = () => {
-    return (
-        <div>
-            hello from rooms page
-        </div>
-    )
-}
-export default Rooms
+  return <Hero hero="roomsHero" />;
+};
+export default Rooms;
